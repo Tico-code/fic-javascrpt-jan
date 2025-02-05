954b2702d70c1569e5fb510435a266442f3371f8
Usuário <thierrystudying@gmail.com>
2025-02-05 15:45:17 -0300

    Adicionando arquivo javascript

---
+++ 05-02/calc.js
@@ -0,0 +1,11 @@
+// duas variáveis
+// num1 e num2
+// pergunta para o usuário
+let num1 = Number(promp ("digite um número"));
+let num2 = Number( prompt("digite outro número"));
+
+console.log(typeof num1);
+console.log(typeof num2);
+
+//imprimir resposta
+console.log(`A soma é ${num1 + num2}`);
