4b45cd1e537dc8a27867a11fa44a5f8bfbf7d929
Usuário <thierrystudying@gmail.com>
2025-02-05 14:53:31 -0300

    JavaScript baby

---
+++ 05-02/script.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Qual seu nome?");
+let idade = prompt ("Qual sua idade?");
+let time = prompt ("Digite seu time");
+
+document.write ("<br><br>");
+document.write(`Seja bem vindo: ${nome}, sua idade é: ${idade} anos, Seu time é ${time}`);
