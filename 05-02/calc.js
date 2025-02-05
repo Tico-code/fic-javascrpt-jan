4987b8979ab9de6f8399624934c2b64b2b19cf62
Usuário <thierrystudying@gmail.com>
2025-02-05 16:18:32 -0300

    Exércicio

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -1,11 +1,19 @@
 // duas variáveis
 // num1 e num2
 // pergunta para o usuário
-let num1 = Number(promp ("digite um número"));
-let num2 = Number( prompt("digite outro número"));
+let num1 = Number (prompt ("digite um número"));
+let num2 = Number ( prompt("digite outro número"));
 
-console.log(typeof num1);
-console.log(typeof num2);
+// operação
+let soma = num1 + num2;
+let sub = num1 - num1;
+let mult = num1 * num2;
+let div = num1 / num2;
+
 
 //imprimir resposta
-console.log(`A soma é ${num1 + num2}`);
+
+console.log(`A soma é ${soma}`);
+console.log(`A subtração é ${sub}`);
+console.log(`A Multiplicação é ${mult}`);
+console.log(`A divisão é ${div}`);
