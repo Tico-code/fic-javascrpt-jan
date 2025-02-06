1db4913bc389784f1d0fdb4563eae465cb916948
Usuário <thierrystudying@gmail.com>
2025-02-06 14:05:19 -0300

    adicionando correção do exer1

---
+++ 06-02/exer1.js
@@ -0,0 +1,8 @@
+console.log ("olá")
+let tom = "Tom";
+let firsttom = "hanks";
+let secondtom = "cruise";
+
+//let myFavoriteTom = tom + ' ' + firsttom + ' ' + secondtom
+let myFavoriteTom = `${tom} ${firsttom} ${secondtom} `;
+console.log(myFavoriteTom);
