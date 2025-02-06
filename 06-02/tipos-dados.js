e3d4abc91c1edd684108aa326ec9d8bb3833db44
Usuário <thierrystudying@gmail.com>
2025-02-06 14:42:00 -0300

    Tipos de dados

---
+++ 06-02/tipos-dados.js
@@ -0,0 +1,19 @@
+// string
+let nome = "Thierry"
+
+//inteiro
+let idade = 18;
+
+//Boolean
+let casado = false;
+
+//números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+//array
+let linguagens = ["css" , "python"]
+
+console.log("Meu nome é " , nome);
+console.log("Idade? " , idade);
+console.log("Casado? " , casado);
+console.log("R$ " , valorProduto);
---
+++ 06-02/variavel.js
@@ -0,0 +1,8 @@
+let mensagem = "Olá, Mundo";
+const nomeCompleto = "Thierry Conceição Costa";
+let idade = 17;
+
+idade = 18;
+
+console.log(nomeCompleto);
+console.log (`${mensagem} minha idade é ${idade}`);
