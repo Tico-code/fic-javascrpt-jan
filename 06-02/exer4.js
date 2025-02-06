c6f3dafc456e2df2c9385414785f1aa039391119
Usuário <thierrystudying@gmail.com>
2025-02-06 15:49:37 -0300

    adicionando exer5

---
+++ 06-02/exer4.js
@@ -0,0 +1,6 @@
+const mozilla = "Mozi Developer Network";
+
+// MDN
+
+const mdn = mozilla[0] + mozilla [5] + mozilla [15]
+console.log(mdn)
---
+++ 06-02/exer5.js
@@ -0,0 +1,6 @@
+/*
+    - exiba no console do browser a quantidade de caracteres que a constante possui
+*/
+
+const linguagem = "JavaScript"
+console.log(linguagem.length)
