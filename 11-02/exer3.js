f5ca6fba08400e43f3844f78f1118b116995f2f8
Usuário <thierrystudying@gmail.com>
2025-02-11 16:17:33 -0300

    adicionando arquivo de exércicio!

---
+++ 11-02/exer3.js
@@ -0,0 +1,10 @@
+const idade1 = 15;
+
+const váriavel = idade <= 7;
+const váriavel2 = idade >= 65;
+
+if (idade1 = váriavel || váriavel2) {
+    console.log("Para você a entra é grátis");
+} else {
+    console.log("A entrada é R$ 30,00.");
+}
