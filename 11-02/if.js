aabd6b811b8fcb9f6e1cc15b1440bf481326aba9
Usuário <thierrystudying@gmail.com>
2025-02-11 14:04:52 -0300

    feat: adicionando else if

--- 11-02/if.js
+++ 11-02/if.js
@@ -2,12 +2,12 @@
     Se a idade for maior ou igual a 18, a pessoa pode ir para a festa
 */
 
-let idade = 37;
+let idade = 17;
 
 if (idade >= 18) {
-console.log("Você é maior de idade")
+console.log("Você é maior de idade");
 
+} else {
+    console.log("você não pode ir a festa");
 }
-if (idade <= 18) {
-    console.log("você não pode ir a festa")
-}
+ 
