251417ce5378f90d44913a5a48c97a74e429be3f
Usuário <thierrystudying@gmail.com>
2025-02-11 14:46:11 -0300

    adicionando nota de aluno

---
+++ 11-02/nota.js
@@ -0,0 +1,12 @@
+/*
+    se a nota do aluno for maior ou igual a 6, o aluno está aprovado
+    senão, O aluno está reprovado
+*/
+
+let nota = 5;
+
+if (nota >= 6) {
+    console.log("Você está aprovado")
+} else {
+    console.log("Você está reprovado")
+}
