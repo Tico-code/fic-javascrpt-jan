420422d57ddfd343e15d51b506ec50e7a49c6f87
Usuário <thierrystudying@gmail.com>
2025-02-12 14:00:30 -0300

    adding again the the age variable...

--- 11-02/exer3.js
+++ 11-02/exer3.js
@@ -1,6 +1,8 @@
-let age = 17;
+const age = 6;
+const isAgeFirst = age <=7;
+const isAgeLast = age >=65;
 
-if (age <= 7 || age >= 65) {
+if (isAgeFirst || isAgeLast) {
 console.log(`Para você, a entrada é grátis! wow`)
 } else {
     console.log(`A entrada é 30 trilhões de dólares`)
