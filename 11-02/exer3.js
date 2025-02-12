ccea019c5b786696caa519b426f3354d2cdc405c
Usuário <thierrystudying@gmail.com>
2025-02-12 13:53:22 -0300

    adding the exercise 3 about variable and age

--- 11-02/exer3.js
+++ 11-02/exer3.js
@@ -1,10 +1,7 @@
-const idade1 = 15;
+let age = 17;
 
-const váriavel = idade <= 7;
-const váriavel2 = idade >= 65;
-
-if (idade1 = váriavel || váriavel2) {
-    console.log("Para você a entra é grátis");
+if (age <= 7 || age >= 65) {
+console.log(`Para você, a entrada é grátis! wow`)
 } else {
-    console.log("A entrada é R$ 30,00.");
+    console.log(`A entrada é 30 trilhões de dólares`)
 }
