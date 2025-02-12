ea33ab59f6f9c41a9b5a25ba8626db2f10040cc3
Usuário <thierrystudying@gmail.com>
2025-02-12 14:19:56 -0300

    Adding the decrementation, as division, multiplicatin, Minus

---
+++ 12-02/decremento.js
@@ -0,0 +1,17 @@
+let valor = 7;
+
+valor = valor - 1;
+valor--;
+
+console.log(valor);
+
+/* multiplicação */
+
+valor *= 2;
+console.log(valor);
+
+
+/* Divisão */
+
+valor /= 4;
+console.log(valor);
