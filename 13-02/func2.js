6563f75ae9ded28e6b97e1a6cef80dc322640ac8
Usuário <thierrystudying@gmail.com>
2025-02-13 16:20:03 -0300

    adding new exercise incomplet

---
+++ 13-02/func2.js
@@ -0,0 +1,13 @@
+function mostrarNome(nome) {
+    document.write(nome);
+}
+
+function pularLinha() {
+    document.write("<br><br>")
+}
+pularLinha();
+mostrarNome("Fábio");
+pularLinha();
+mostrarNome("Káio");
+pularLinha();
+mostrarNome("Carla");
