c537bd642cc58522cfc44c018bf2e936be0523f7
Usuário <thierrystudying@gmail.com>
2025-02-20 14:35:07 -0300

    adding localstorage baby

---
+++ 20-02/js/cadastro.js
@@ -0,0 +1,13 @@
+// Variáveis
+const campo = document.querySelector("#campo");
+const salvarBtn = document.querySelector("#salvar");
+
+// Adicionar evento de clique ao botão salvar
+salvarBtn.addEventListener("click", function() {
+    const valor = campo.value;
+    
+    // salvar no local storage
+
+    const chave = `ValorSalvo_${Date.now()}`; // usa timestamp para chave única
+    console.log(chave)
+});
