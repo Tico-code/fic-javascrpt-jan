e7dacc30b3bb830ad5890634456b4cfde4c9ef17
Usuário <thierrystudying@gmail.com>
2025-02-20 15:41:13 -0300

    Adding the list setup of the LocalStorage key

---
+++ 20-02/js/listagem.js
@@ -0,0 +1,19 @@
+const listaValores = document.querySelector("#listaValores");
+
+// Carregar valores ao iniciar a página
+carregarValoresSalvos();
+
+function carregarValoresSalvos() {
+    listaValores.innerHTML = ""; // limpar a lista antes de adicionar os valores
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const chave = localStorage.key(i);
+        if (chave.startsWith("ValorSalvo")) { // filtra as chaves desejadas
+            const valor = localStorage.getItem(chave);
+                //criar um elemento HTML    
+            const listItem = document.createElement("li");
+            listItem.textContent = valor;
+            listaValores.appendChild(listItem)
+        }
+    }
+}
