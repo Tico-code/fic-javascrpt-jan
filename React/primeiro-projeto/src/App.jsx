8b48b842faf9603bd7c5e3d645f61c7c019f7c8c
Usuário <thierrystudying@gmail.com>
2025-02-25 16:06:39 -0300

    Adding react babe

--- React/primeiro-projeto/src/App.jsx
+++ React/primeiro-projeto/src/App.jsx
@@ -1,3 +1,7 @@
+import Header from "./Components/Header";
+
+
+
 
 
 function App() {
@@ -6,10 +10,9 @@ function App() {
   return (
     <>
      <div classname="container">
-      <header>
+      <Header />
         <h1>Logo</h1>
-      </header>
-      
+
       <h1> Seja Bem-vindo</h1>
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quas unde enim? Reiciendis adipisci, amet quo, qui nihil suscipit laboriosam assumenda ea dicta dolorem autem illum quia quae consequatur quibusdam!</p>
