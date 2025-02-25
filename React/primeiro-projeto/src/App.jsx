5b9b3bdce8d4bc9df9745a0b7e780f07d32768a7
Usuário <thierrystudying@gmail.com>
2025-02-25 16:01:56 -0300

    adding

--- React/primeiro-projeto/src/App.jsx
+++ React/primeiro-projeto/src/App.jsx
@@ -5,9 +5,16 @@ function App() {
 
   return (
     <>
-      <p>
-        Thierry
-      </p>
+     <div classname="container">
+      <header>
+        <h1>Logo</h1>
+      </header>
+      
+      <h1> Seja Bem-vindo</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quas unde enim? Reiciendis adipisci, amet quo, qui nihil suscipit laboriosam assumenda ea dicta dolorem autem illum quia quae consequatur quibusdam!</p>
+
+     </div>
     </>
   )
 }
