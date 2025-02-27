12bb0f2e717f871d674c11569b33f3e53f896b83
Usuário <thierrystudying@gmail.com>
2025-02-27 16:02:20 -0300

    adding some fix and card component

--- React/primeiro-projeto/src/App.jsx
+++ React/primeiro-projeto/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import img1 from "./assets/img-1.jpg";
 import img2 from "./assets/img-2.jpg";
+import Card from "./Components/Card";
 import "./App.css";
 
 
@@ -20,9 +21,13 @@ function App() {
       <h1>Welcome to my site</h1>
       <p> Here you can find the bestest offerts</p>
     </Banner>
-    
+    <div>
     < ImgCard caption= "uma imagem legal" newImg= {img1}/>
     < ImgCard caption= "uma imagem legal" newImg= {img2}/>
+    < ImgCard caption= "uma imagem legal" newImg= {img2}/>
+    </div>
+
+    <Card />
     
      </div>
     </>
---
+++ React/primeiro-projeto/src/Components/Card/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style.css"
+
+export default function Card() {
+  return (
+    <div className="card-container">
+        <div className="card">
+            <h2>Nome title</h2>
+            <p> texto texto novo texo tico.</p>
+            <a href="#"> Saiba mais</a>
+        </div>
+    </div>
+  )
+}
