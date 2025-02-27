646b5e367f67f354fc2e46f623cb2f8705e0e6d8
Usuário <thierrystudying@gmail.com>
2025-02-27 16:25:53 -0300

    adding new things in card

--- React/primeiro-projeto/src/Components/Card/index.jsx
+++ React/primeiro-projeto/src/Components/Card/index.jsx
@@ -2,13 +2,43 @@ import React from 'react'
 import "./style.css"
 
 export default function Card() {
+
+    const lista = [
+        {
+          nome: "Projeto novo",
+          responsavel: "Fernando Carlos",
+          valor: 1200
+        },
+        {
+          nome: "Projeto do Mar",
+          responsavel: "Fonseca",
+          valor: 4000
+        },
+        {
+          nome: "Giovanni",
+          responsavel: "Pedro",
+          valor: 2000
+        },
+        {
+          nome: "Fernanda",
+          responsavel: "Pedro",
+          valor: 990
+        }
+      ];
+
   return (
     <div className="card-container">
-        <div className="card">
-            <h2>Nome title</h2>
-            <p> texto texto novo texo tico.</p>
-            <a href="#"> Saiba mais</a>
+        {lista.map((item, index) => {
+            return (
+        <div className="card" key={index}>
+            <h2>{item.nome}</h2>
+            <p>{item.responsavel}</p>
+            <strong>{item.valor}</strong>
+            <hr />
         </div>
+            )
+        })}
+        
     </div>
   )
 }
